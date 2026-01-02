1f0037a19398bc032dff189b1cc969b1a55d9a93
David Hamner <ke7oxh@gmail.com>
2026-01-01 16:26:44 -0800

    Simplify password requirements - just 8 chars minimum

--- routes/auth.py
+++ routes/auth.py
@@ -124,15 +124,9 @@ def validate_username(username):
 
 
 def validate_password(password):
-    """Validate password strength."""
+    """Validate password strength - just 8 chars minimum for simplicity."""
     if len(password) < 8:
-        return False, "Password must be at least 8 characters long"
-    if not re.search(r"[A-Z]", password):
-        return False, "Password must contain at least one uppercase letter"
-    if not re.search(r"[a-z]", password):
-        return False, "Password must contain at least one lowercase letter"
-    if not re.search(r"\d", password):
-        return False, "Password must contain at least one digit"
+        return False, "Password must be at least 8 characters"
     return True, None
 
 
